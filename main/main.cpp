e13cff4e1bafc1ad5a65f01a8c2e6d16b1c82e54
Patrick Brennan <brennanpatrick20@gmail.com>
2026-04-02 19:42:10 -0400

    Create websocket for joystick stuff and add joystick stuff

--- main/main.cpp
+++ main/main.cpp
@@ -8,6 +8,14 @@
 #include "interrupt.hpp"
 #include "led.hpp"
 #include <atomic>
+#include "task.hpp"
+#include "wifi_ap.hpp"
+#include <esp_http_server.h>
+#include "memory.h"
+
+#if CONFIG_ESP32_WIFI_NVS_ENABLED
+#include "nvs_flash.h"
+#endif
 
 #define BUTTON_PIN GPIO_NUM_18
 #define ENC_INTERRUPT_PIN GPIO_NUM_14
@@ -22,10 +30,31 @@
 
 #define ADC_ATTEN ADC_ATTEN_DB_12
 
+// GOAL LIST!!!!!!!!
+// Welcome to the great bodge because we too lazy to figure out USB HID stuff
+// -> WiFi AP
+//     -> Wireless AP, connectable
+// -> Websocket server
+//     -> Connect from game
+//     -> accept requests from game, taking in:
+//          -> New setpoints
+//     -> return system data:
+//          -> Setpoint
+//          -> Live RPM
+//          -> How many times button has been pressed since last request
+//          -> Joystick values
+
 using namespace std::chrono_literals;
 
+// PI controller parameters
+const std::chrono::milliseconds sample_period(50); // 100 ms loop
+float kp = 0.0003f; // proportional gain (tune as needed)
+float ki = 0.0009f; // integral gain (tune as needed)
+float integrator = 0.0f;
+const float integrator_limit = 1000.0f;
+std::atomic<float> target_rpm = 0.0f; // desired RPM (adjust as needed)
+
 static espp::Logger logger({.tag = "ESP32", .level = espp::Logger::Verbosity::DEBUG});
-static int adc_raw[2][10];
 
 static auto last = std::chrono::high_resolution_clock::now();
 static float speed{0.0f};
@@ -33,6 +62,15 @@ static float rpm{0.0f};
 
 static double weights[SMOOTH_SIZE];
 static std::vector<double> rpm_buffer;
+
+int adc_ch4;
+int adc_ch8;
+
+uint16_t y_axis;
+uint16_t x_axis;
+
+httpd_handle_t server;
+httpd_config_t config;
 
 template <typename T,typename M> T deadzone(T val, M deadzone) {
     if (val > deadzone || val < -deadzone) return val;
@@ -91,30 +129,146 @@ double smooth_single(double sample) {
   return (weightSum > 0.0) ? result / weightSum : sample;
 }
 
+struct packet_data {
+  float target_rpm;
+  double x_axis;
+  double y_axis;
+};
+
+static esp_err_t ws_handler(httpd_req_t *req) {
+
+  // logger.info("recc'd request or something");
+  httpd_ws_frame_t ws_pkt;
+  uint8_t *buf = NULL;
+  memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
+  ws_pkt.type = HTTPD_WS_TYPE_TEXT;
+
+  // this has to be here otherwise it doesn't work
+  if (req->method == HTTP_GET) {
+    logger.info("Handshake!!!!");
+    return ESP_OK;
+  }
+
+  /* Set max_len = 0 to get the frame len */
+  esp_err_t ret = httpd_ws_recv_frame(req, &ws_pkt, 0);
+  if (ret != ESP_OK) {
+      logger.error("httpd_ws_recv_frame failed to get frame len with {}", ret);
+      return ret;
+  }
+
+  // logger.info("Frame type: {}", (int) ws_pkt.type);
+
+  // logger.info("pkt length: {}", ws_pkt.len);
+  if (ws_pkt.len) {
+      buf = (uint8_t *)calloc(1, ws_pkt.len+1);
+      ws_pkt.payload = buf;
+      /* Set max_len = ws_pkt.len to get the frame payload */
+      ret = httpd_ws_recv_frame(req, &ws_pkt, ws_pkt.len);
+      if (ret != ESP_OK) {
+          logger.error("httpd_ws_recv_frame failed with {}", ret);
+          free(buf);
+          return ret;
+      }
+      // logger.info("Got packet with message: {}", *ws_pkt.payload);
+  }
+  //! STuff needs to happen here or something
+  // future self, you may do that for me if you would :)
+  // logger.info("Packet type: {}", ws_pkt.type);
+  if (ws_pkt.type == HTTPD_WS_TYPE_BINARY &&
+    ws_pkt.payload != NULL) {
+    // logger.info("got packet: {}", ws_pkt.payload[1]);
+    if (ws_pkt.payload[0] == 0) {
+      // logger.info("requested data!!!");
+      uint8_t return_payload[4];
+      return_payload[0] = (uint8_t) x_axis & 0x00FF;
+      return_payload[1] = (uint8_t) ((x_axis & 0xFF00) >> 8);
+      return_payload[2] = (uint8_t) y_axis & 0x00FF;
+      return_payload[3] = (uint8_t) ((y_axis & 0xFF00) >> 8);
+      ws_pkt.payload = return_payload;
+      ws_pkt.len = sizeof(return_payload);
+    }
+  }
+
+  ret = httpd_ws_send_frame(req, &ws_pkt);
+  if (ret != ESP_OK) {
+      logger.error("httpd_ws_send_frame failed with %d", ret);
+  }
+  free(buf);
+  return ret;
+}
+
+static const httpd_uri_t ws = {
+        .uri        = "/ws",
+        .method     = HTTP_GET,
+        .handler    = ws_handler,
+        .user_ctx   = NULL,
+        .is_websocket = true
+};
+
+// Webserver stuff
+void start_webserver(void) {
+  config = HTTPD_DEFAULT_CONFIG();
+
+  // Start the httpd server
+  logger.info("Starting server on port: '{}'", config.server_port);
+  if (httpd_start(&server, &config) == ESP_OK) {
+      // Registering the ws handler
+      logger.info("Registering URI handlers");
+      ESP_ERROR_CHECK_WITHOUT_ABORT(httpd_register_uri_handler(server, &ws));
+      logger.info("Done.");
+      return;
+  }
+
+  logger.error("Error starting server!");
+}
+
 // MAIN STUFF =-=-=-=-=--=-
 
 extern "C" void app_main(void) {
 
-  // The button
-  ESP_ERROR_CHECK(gpio_input_enable(BUTTON_PIN));
-  ESP_ERROR_CHECK(gpio_set_pull_mode(BUTTON_PIN, GPIO_PULLDOWN_ONLY));
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+  #if CONFIG_ESP32_WIFI_NVS_ENABLED
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+  #endif
+
+  // Wifi for game
+  espp::WifiAp::Config ap_config{.ssid = "youjustlost",
+                                .password = "thegame",
+                                .log_level = espp::Logger::Verbosity::DEBUG};
+  espp::WifiAp wifi_ap(ap_config);
+
+
+  // websocket for game
+  start_webserver();
 
   // ADC configuration
 
-  adc_oneshot_chan_cfg_t config = {
+  adc_oneshot_chan_cfg_t adc_config = {
     .atten = ADC_ATTEN,
     .bitwidth = ADC_BITWIDTH_12,
   };
 
-  adc_oneshot_unit_handle_t adc2_handle;
-  adc_oneshot_unit_init_cfg_t init_config2 = {
-    .unit_id = ADC_UNIT_2,
+  adc_oneshot_unit_handle_t adc1_handle;
+  adc_oneshot_unit_init_cfg_t init_config1 = {
+    .unit_id = ADC_UNIT_1,
   };
-  ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config2, &adc2_handle));
+  ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
 
-  ESP_ERROR_CHECK(adc_oneshot_config_channel(adc2_handle, ADC_CHANNEL_8, &config));
-  ESP_ERROR_CHECK(adc_oneshot_config_channel(adc2_handle, ADC_CHANNEL_4, &config));
+  ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_0, &adc_config)); // VP pin
+  ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_3, &adc_config)); // VN pin
 
+  ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_CHANNEL_0, &adc_ch4));
+
+  // The button
+  ESP_ERROR_CHECK(gpio_input_enable(BUTTON_PIN));
+  ESP_ERROR_CHECK(gpio_set_pull_mode(BUTTON_PIN, GPIO_PULLDOWN_ONLY));
 
   // Encoder interrupt stuff
 
@@ -174,38 +328,16 @@ extern "C" void app_main(void) {
   // Compute weights for smoothing
   compute_weights();
 
-  // PI controller parameters
-  const std::chrono::milliseconds sample_period(50); // 100 ms loop
-  float kp = 0.0003f; // proportional gain (tune as needed)
-  float ki = 0.0009f; // integral gain (tune as needed)
-  float integrator = 0.0f;
-  const float integrator_limit = 1000.0f;
-  float target_rpm = 500.0f; // desired RPM (adjust as needed)
-
   bool button_pressed = false;
 
-  while (1) {
-
-    // joystick stuff
-
-    int adc_ch4;
-    int adc_ch8;
-
-    ESP_ERROR_CHECK(adc_oneshot_read(adc2_handle, ADC_CHANNEL_4, &adc_ch4));
-    ESP_ERROR_CHECK(adc_oneshot_read(adc2_handle, ADC_CHANNEL_8, &adc_ch8));
-
-    double y_axis = map_range(adc_ch4, Y_MIN, Y_MAX, -4096, 4096)/4096.0;
-    double x_axis = map_range(adc_ch8, X_MIN, X_MAX, -4096, 4096)/4096.0;
-
-    y_axis = deadzone(y_axis, 0.10);
-    y_axis = deadzone(y_axis, 0.10);
-
-    // spinner stuff
-
+  // Create task for running the PI loop
+  // not entirely thread safe but honestly should be fiiiiiinne
+  espp::Task::configure_task_watchdog(1000ms, true);
+  auto pi_periodic = [](std::mutex &m, std::condition_variable &cv) {
     rpm = ((speed / ENC_CPR) * 60.0f);
 
     float measured_rpm = smooth_single(rpm);
-    float error = target_rpm - measured_rpm;
+    float error = target_rpm.load() - measured_rpm;
     // integrate error (simple forward Euler)
     integrator += error * (sample_period.count() / 1000.0f);
     if (integrator > integrator_limit) integrator = integrator_limit;
@@ -220,6 +352,36 @@ extern "C" void app_main(void) {
     output = (output * (1.0 - 0.18)) + 0.18;
 
     set_motor_speed(output);
+
+    // logger.info("Setpoint: {}, raw_rpm: {:.2f}, rpm: {}", target_rpm, rpm, measured_rpm);
+
+    // Return false so task keeps running
+    //threadsafe wait
+    std::unique_lock<std::mutex> lk(m);
+    cv.wait_for(lk, sample_period);
+
+    return false;
+  };
+
+  auto pi_task = espp::Task({.callback = pi_periodic,
+                          .task_config = {.name = "PILoop"},
+                          .log_level = espp::Logger::Verbosity::DEBUG});
+  pi_task.start();
+  pi_task.start_watchdog();
+
+
+  while (1) {
+
+    // joystick stuff
+
+    ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_CHANNEL_0, &adc_ch4));
+    ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_CHANNEL_3, &adc_ch8));
+
+    y_axis = map_range(adc_ch4, Y_MIN, Y_MAX, 0, 65535);
+    x_axis = map_range(adc_ch8, X_MIN, X_MAX, 0, 65535);
+
+    // y_axis = deadzone(y_axis, 0.10);
+    // y_axis = deadzone(y_axis, 0.10);
 
     // Set so code only runs when button is first pressed and doesn't loop while pressed
     // can probably be replaced with an interupt if need bee
@@ -241,8 +403,8 @@ extern "C" void app_main(void) {
       button_pressed = false;
     }
 
-    logger.info("Setpoint: {}, RAW_RPM: {}, RPM: {}, Out: {}, output_rpm: {}, yaxis: {:.2f}, yaxisraw: {}", target_rpm, rpm, measured_rpm, output, output * 1600.0, y_axis, adc_ch4);
+    logger.info("yaxis: {}, xaxis: {}", y_axis, x_axis);
 
-    std::this_thread::sleep_for(sample_period);
+    std::this_thread::sleep_for(10ms);
   }
 }
